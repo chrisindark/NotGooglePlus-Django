15d1b57cc894f382749cb12420de4e710e1ebc97
Christopher Paul <christopherp@mindfiresolutions.com>
2017-05-12 19:13:25 +0530

    updated configuration

--- notgoogleplus/wsgi.py
+++ notgoogleplus/wsgi.py
@@ -5,6 +5,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "notgoogleplus.settings.development")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "notgoogleplus.settings")
 
 application = get_wsgi_application()
