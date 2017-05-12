d59867bffc2509845481d72f96d866f9dbdcf219
Christopher Paul <christopherp@mindfiresolutions.com>
2017-05-12 19:15:16 +0530

    updated configuration

--- notgoogleplus/settings.py
+++ notgoogleplus/settings.py
@@ -214,6 +214,7 @@ INSTALLED_APPS += (
 MIDDLEWARE = ('notgoogleplus.middleware.QueryCountDebugMiddleware',) + MIDDLEWARE
 
 db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES = {}
 DATABASES['default'].update(db_from_env)
 
 CORS_ORIGIN_ALLOW_ALL = True
