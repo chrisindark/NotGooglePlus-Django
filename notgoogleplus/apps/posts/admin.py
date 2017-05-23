0e6a166824a2196e4ed7b09efd322a2df289c76a
Christopher Paul <chrisindark.notafraid@gmail.com>
2017-05-23 11:57:49 +0530

    updated code

--- notgoogleplus/apps/posts/admin.py
+++ notgoogleplus/apps/posts/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Post, PostComment
+from .models import Post, PostComment, File
 
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(PostComment)
+admin.site.register(File)
