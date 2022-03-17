55a7bf78f1997c0d87cd9a1f9e2b0a2d4a0e8177
Christopher Paul <christopher.paul@trell.in>
2022-03-18 03:42:24 +0530

    updated code

--- apps/core/tasks.py
+++ apps/core/tasks.py
@@ -1,11 +1,19 @@
 from celery import shared_task
 
-from apps.core.sqsutility import SqsUtility
+# from utils.mail import api_send_mail
+
+# from apps.core.sqsutility import SqsUtility
+from notgoogleplus.celery import app
 
 
 @shared_task
 def add(x, y):
     return x + y
+
+
+@app.task
+def subtract(x, y):
+    return x - y
 
 
 @shared_task
@@ -18,7 +26,33 @@ def xsum(numbers):
     return sum(numbers)
 
 
-sqs_utility = SqsUtility()
+def backoff(attempts):
+    """Return a backoff delay, in seconds, given a number of attempts.
+
+    The delay increases very rapidly with the number of attemps:
+    1, 2, 4, 8, 16, 32, ...
+
+    """
+    return 2 ** attempts
+
+
+class BaseTask(app.Task):
+    """Abstract base class for all tasks in my app."""
+
+    abstract = True
+
+    def on_retry(self, exc, task_id, args, kwargs, einfo):
+        """Log the exceptions to sentry at retry."""
+        # sentrycli.captureException(exc)
+        super(BaseTask, self).on_retry(exc, task_id, args, kwargs, einfo)
+
+    def on_failure(self, exc, task_id, args, kwargs, einfo):
+        """Log the exceptions to sentry."""
+        # sentrycli.captureException(exc)
+        super(BaseTask, self).on_failure(exc, task_id, args, kwargs, einfo)
+
+
+# sqs_utility = SqsUtility()
 
 
 # @shared_task
@@ -35,3 +69,40 @@ sqs_utility = SqsUtility()
 #         sqs_utility.delete_message(
 #             queue_url=queue_url,
 #             receipt_handle=response['Messages'][i]['ReceiptHandle'])
+
+
+@app.task(bind=True, default_retry_delay=300, max_retries=3, soft_time_limit=5, base=BaseTask)
+def send_mail(self, recipients, sender_email, subject, body):
+    """Send a plaintext email with argument subject, sender and body to a list of recipients."""
+    try:
+        # data = api_send_mail(recipients, sender_email, subject, body)
+        pass
+    except Exception as e:
+        # No need to retry as the user provided an invalid input
+        raise
+    except Exception as exc:
+        # Any other exception. Log the exception to sentry and retry in 10s.
+        # sentrycli.captureException()
+        self.retry(countdown=10, exc=exc)
+    # return data
+
+
+class Handle_Event(BaseTask):
+    def validate_input(self, event):
+        pass
+
+    def get_or_create_model(self, event):
+        pass
+
+    def stream_event(self, event):
+        pass
+
+    def run(self, event):
+        if not self.validate_input(event):
+            raise Exception(event)
+        try:
+            model = self.get_or_create_model(event)
+        except Exception as exc:
+            self.retry(countdown=backoff(self.request.retries), exc=exc)
+        else:
+            self.stream_event(event)
--- notgoogleplus/celery.py
+++ notgoogleplus/celery.py
@@ -1,34 +1,104 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals, absolute_import
-
+from datetime import timedelta
 import os
 
 from celery import Celery
-from celery.schedules import crontab
-
 from django.conf import settings
+from celery.schedules import crontab
+from kombu import Queue, Exchange
 
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'notgoogleplus.settings.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'notgoogleplus.settings.development')
 
 app = Celery('notgoogleplus')
 # Using a string here means the worker will not have to
 # serialize the configuration object to child processes.
 # namespace='CELERY' means all celery-related configuration keys
 # should have a `CELERY_` prefix.
-app.config_from_object('django.conf:settings')
+app.config_from_object('django.conf:settings', namespace="CELERY")
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app.conf.timezone = 'UTC'
+app.conf.task_default_queue = 'default'
+app.conf.tasks_queues = (
+    Queue('default', exchange='default', routing_key='default'),
+    Queue('queue1', exchange='queue1', routing_key='queue1'),
+    Queue('queue2', exchange='queue2', routing_key='queue2'),
+)
+
+# _ALLOWED_QUEUES = ('default', 'queue1', 'queue2')
+# task_queues = tuple(Queue(name=q, exchange=Exchange(q), routing_key=q)
+#                     for q in _ALLOWED_QUEUES)
+
+app.conf.task_routes = {
+    'apps.core.tasks.subtract': {
+        'queue': 'default',
+        'routing_key': 'default'
+    },
+    'apps.core.tasks.add': {
+        'queue': 'queue1',
+        'routing_key': 'queue1'
+    },
+    'apps.core.tasks.mul': {
+        'queue': 'queue2',
+        'routing_key': 'queue2'
+    },
+}
 app.conf.beat_schedule = {
     'add-every-5-seconds': {
         'task': 'apps.core.tasks.add',
-        'schedule': crontab(),
+        'schedule': timedelta(seconds=5),
         'args': (20, 20),
     },
-    #'process-sqs-messages-test_queue': {
+    'subtract-every-1-minute': {
+        'task': 'apps.core.tasks.subtract',
+        'schedule': crontab(minute='*'),
+        'args': (20, 20),
+    },
+    # 'process-sqs-messages-test_queue': {
     #    'task': 'apps.core.tasks.process_sqs_messages',
     #    'schedule': 1.0
-    #}
+    # }
 }
-app.conf.timezone = 'UTC'
+
+# CELERY_QUEUES = (
+#     Queue('default', exchange='default', routing_key='default'),
+#     Queue('queue1', exchange='queue1', routing_key='queue1'),
+#     Queue('queue2', exchange='queue2', routing_key='queue2'),
+# )
+# CELERY_ROUTES = {
+#     'apps.core.tasks.subtract': {
+#         'queue': 'default',
+#         'routing_key': 'default'
+#     },
+#     'apps.core.tasks.add': {
+#         'queue': 'queue1',
+#         'routing_key': 'queue1'
+#     },
+#     'apps.core.tasks.mul': {
+#         'queue': 'queue2',
+#         'routing_key': 'queue2'
+#     },
+# }
+# CELERY_BEAT_SCHEDULE = {
+#     'add-every-5-seconds': {
+#         'task': 'apps.core.tasks.add',
+#         'schedule': timedelta(seconds=5),
+#         'args': (20, 20),
+#     },
+#     'subtract-every-1-minute': {
+#         'task': 'apps.core.tasks.subtract',
+#         'schedule': crontab(minute='*'),
+#         'args': (20, 20),
+#     },
+# }
+
+# commands to run a single worker for all queues or separate workers for separate queues
+# celery -A notgoogleplus worker -E -l INFO --autoscale 1,8 -n worker1 -Q default,queue1,queue2
+# celery -A notgoogleplus worker -E -l INFO --autoscale 1,8 -n worker1 -Q default
+# celery -A notgoogleplus worker -E -l INFO --autoscale 1,8 -n worker2 -Q queue1
+# celery -A notgoogleplus worker -E -l INFO --autoscale 1,8 -n worker3 -Q queue2
+
+# command to run celery beat for cron jobs
+# celery -A notgoogleplus beat -l INFO
